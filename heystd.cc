f7b57bc9cd0aaad0e86f4d304690ce620dcf8144
Dustin <asight4soreyes@gmail.com>
2021-07-04 18:27:52 -0400

    code before did not work

--- heystd.cc
+++ heystd.cc
@@ -5,7 +5,7 @@ using namespace std;
 int main () {
 
    // std::cout << "Hello There Human looking at me! " << std::endl;
-cout << "Hello There Human looking at me!\ 
+cout << "Hello There Human looking at me!\
  This message was printed using std C " << endl;
 
   return 0;
