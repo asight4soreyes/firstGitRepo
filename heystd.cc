506dccddb98b3b3c1ac16d688ab43bbea17653fb
Dustin <asight4soreyes@gmail.com>
2021-07-04 13:12:16 -0400

    added basic std code

---
+++ heystd.cc
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+   // std::cout << "Hello There Human looking at me! " << std::endl;
+    cout << "Hello There Human looking at me! 
+       This message was printed using std C " << endl;
+
+  return 0;
+}
